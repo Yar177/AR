c59f5274d7aef35eaaec8c894405a4d99953a549
Yar <yarsher.dev@gmail.com>
2019-04-10 16:31:40 -0400

    CustomRenderer

---
+++ app/src/main/java/com/yar/ar/CustomRenderer.java
@@ -0,0 +1,24 @@
+package com.yar.ar;
+
+import javax.microedition.khronos.opengles.GL10;
+
+import edu.dhbw.andar.interfaces.OpenGLRenderer;
+
+public class CustomRenderer implements OpenGLRenderer {
+
+
+    @Override
+    public void draw(GL10 gl10) {
+
+    }
+
+    @Override
+    public void setupEnv(GL10 gl10) {
+
+    }
+
+    @Override
+    public void initGL(GL10 gl10) {
+
+    }
+}
